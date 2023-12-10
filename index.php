e4a5849c2bda5edec61dba470d050e4dddd1bd9a
Than Quang Phuong <142188056+PhuongThan25503@users.noreply.github.com>
2023-12-10 22:41:20 +0700

    okay

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// How controllers call Views & Models
+require_once "./core/BaseController.php";
+
+// Connect Database
+require_once "./core/DB.php";
+
+
+$controllerName = ucfirst($_REQUEST['controller']?? 'Login'). 'Controller';
+
+require_once "./Controllers/${controllerName}.php";
